8c494979bf7e7459830135922b108d82af687011
MileneBackJuwer <48599182+MileneBackJuwer@users.noreply.github.com>
2020-10-13 16:16:42 -0300

    Create educacao.php

---
+++ educacao.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+         <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+        
+    </head>
+    <body class="grey lighten-5">
+    <nav style="height:60px;" class="teal lighten-2">
+    <div class="nav-wrapper">
+      <a href="index.php" class="brand-logo" style="margin-left:15%;"><img src="img/logo.jpg" alt="Voto conciente identidade visual"></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down" style="margin-right:15%; align-contente:conter;">
+        <li><a href="sass.html">Perfil do candidato</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container "style="margin-top:64px;">
+        <div class="col s3">
+        <div class="row">
+        <h2>Acompanhe as propostas dos candidatos a respeito da educação na cidade de Feliz</h2>
+                <div class="section">
+                <h4>Paulo Hahn e Sigrid Huve</h4>
+                </div>
+
+    
+    </body>
+</html>
