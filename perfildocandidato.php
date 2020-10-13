4cc9d674cae9274557ec0fdcdb3122c34b4fa466
MileneBackJuwer <48599182+MileneBackJuwer@users.noreply.github.com>
2020-10-13 16:46:38 -0300

    Create perfildocandidato.php

---
+++ perfildocandidato.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+         <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+        
+    </head>
+    <body class="grey lighten-5">
+    <nav style="height:60px;" class="teal lighten-2">
+    <div class="nav-wrapper">
+      <a href="index.php" class="brand-logo" style="margin-left:15%;"><img src="img/logo.jpg" alt="Voto conciente identidade visual"></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down" style="margin-right:15%; align-contente:conter;">
+        <li><a href="index.php">Perfil do candidato</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container "style="margin-top:64px;">
+        <div class="col s3">
+        <div class="row">
+        <h2>Perfil dos candidatos a prefeito 2020 da cidade de Feliz</h2>
+
+<div class="section">
+               <h4>12 - PDT</h4>     
+  <div class="col s12 m6">
+    <h2 class="header">Paulo Hahn</h2>
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="img/paulo-hahn-12.jpg">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col s12 m6">
+    <h2 class="header">Sigrid Huve</h2>
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="img/sigrid-huve-12.jpg">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+            
+  <div class="section">
+               <h4>12 - PDT</h4>     
+  <div class="col s12 m6">
+    <h2 class="header">Junior Freiberguer</h2>
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="img/paulo-hahn-12.jpg">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col s12 m6">
+    <h2 class="header">Jairo Nienow</h2>
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="img/sigrid-huve-12.jpg">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+            
+  <div class="section">
+               <h4>12 - PDT</h4>     
+  <div class="col s12 m6">
+    <h2 class="header">Nelson Martiny</h2>
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="img/paulo-hahn-12.jpg">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col s12 m6">
+    <h2 class="header">Jorge Zimmer</h2>
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="img/sigrid-huve-12.jpg">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+            
+
+                </div>
+  
+  </body>
+</html>
